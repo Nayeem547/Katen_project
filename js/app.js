2e6472ff88087a0895732cf1b1c38200e7de9a2b
Nayeem547 <106963106+Nayeem547@users.noreply.github.com>
2023-03-03 01:18:40 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".ins_slider").slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+       
+        prevArrow: '<i class="fas fa-chevron-left course_slide_btn_left"></i>',
+        nextArrow: '<i class="fas fa-chevron-right course_slide_btn_right"></i>',
+      });
+
+
+})
